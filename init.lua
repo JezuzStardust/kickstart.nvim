55975f653a1bd94e893df2cc088e8529d640bf9c
Jens Zamanian <jens.zamanian@algoryx.com>
2026-01-29 08:49:26 +0100

    Add ChatGPT plugin.

--- init.lua
+++ init.lua
@@ -380,7 +380,42 @@ require('lazy').setup({
   --
   -- Use `opts = {}` to automatically pass options to a plugin's `setup()` function, forcing the plugin to be loaded.
   --
-
+  {
+    'jackMort/ChatGPT.nvim',
+    event = 'VeryLazy',
+    config = function()
+      require('chatgpt').setup {
+        -- this config assumes you have OPENAI_API_KEY environment variable set
+        openai_params = {
+          -- NOTE: model can be a function returning the model name
+          -- this is useful if you want to change the model on the fly
+          -- using commands
+          -- Example:
+          -- model = function()
+          --     if some_condition() then
+          --         return "gpt-5"
+          --     else
+          --         return "gpt-5-mini"
+          --     end
+          -- end,
+          model = 'gpt-5-mini',
+          -- frequency_penalty = 0,
+          -- presence_penalty = 0,
+          -- max_tokens = 4095,
+          -- temperature = 0.2,
+          -- top_p = 0.1,
+          -- n = 1,
+        },
+        api_key_cmd = nil, -- uses OPENAI_API_KEY env var
+      }
+    end,
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'nvim-lua/plenary.nvim',
+      'folke/trouble.nvim', -- optional
+      'nvim-telescope/telescope.nvim',
+    },
+  },
   -- Alternatively, use `config = function() ... end` for full control over the configuration.
   -- If you prefer to call `setup` explicitly, use:
   --    {
